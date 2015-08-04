ef0c72fea264b638df1d4e38e8741fad21f857bb
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2015-08-04 13:46:55 +0200

    Test Range increases until 300 events.
    run.sh script modified for this automatically.
    Also names the results file with the hostname included.
    Results in lab14 and lab15 inserted.
    There is an error with lab15 GPU allocation.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 resudir="Results"
-resufile="res_"$(date +"%d_%m_%Y_%H:%M").res
+resufile=$(hostname)"_"$(date +"%d_%m_%Y_%H:%M").res
 
 #Check for Results dir
 if ! [ -d resudir ]; then
@@ -11,7 +11,7 @@ if ! [ -d resudir ]; then
 
 echo "Outputs goes to: "$resudir/$resufile
 
-for i in {1..200..10}; do
+for i in {1..20} {30..300..10}; do
     printf "Filtering file for: %d events\n" $i 
     awk -v env=$i '$1=="Event:"{cont++}{if(cont<=env)print $0;else exit 0}' in.dat > /tmp/out.dat
     for a in *.x;do
